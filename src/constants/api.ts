8fe7c59bf017d091f081975cae91612c5338651f
HongSeokChoi <129854575+ChatHongPT@users.noreply.github.com>
2025-08-02 23:45:21 +0900

    fix: api.ts HTTP->HTTPS

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://113.198.66.77:17113';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://113.198.66.77:17113';
 
 export const WS_BASE_URL = process.env.REACT_APP_WS_BASE_URL || 'ws://113.198.66.77:17113';
 
